1b04953fa9f3f8ff69108e2d9cc98a8588c4a3be
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-21 15:47:38 -0600

    cambio variables entorno

--- config/database.js
+++ config/database.js
@@ -2,12 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
+      host: env('PGHOST', 'dpg-cd9g172rrk09hio84u4g-a.oregon-postgres.render.com'),
       port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      database: env('PGDATABASE', 'portafolio_5zae'),
+      user: env('PGUSER', 'tikodev'),
+      password: env('PGPASSWORD', 'FFVt8q7GXdVP1cp9Excd2T7T6IfO1D1k'),
       ssl: env.bool(true),
     },
+    //postgres://tikodev:FFVt8q7GXdVP1cp9Excd2T7T6IfO1D1k@dpg-cd9g172rrk09hio84u4g-a.oregon-postgres.render.com/portafolio_5zae
   },
 });
